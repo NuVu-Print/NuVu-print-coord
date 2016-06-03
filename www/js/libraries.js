693d966f01578e9942d6b5ffea615fefeff9a40b
sdaitzman <samuel@daitzman.com>
2016-06-03 09:59:15 -0400

    frontend progress

--- www/js/libraries.js
+++ www/js/libraries.js
@@ -1,3 +1,4 @@
 //=include ../bower_components/jquery/dist/jquery.min.js
 //=include ../bower_components/bootstrap/dist/js/bootstrap.min.js
 //=include ../bower_components/lodash/dist/lodash.min.js
+//=include ../bower_components/select2/dist/js/select2.full.min.js
