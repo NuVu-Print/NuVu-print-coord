a8808baa37feb0a29ef33d6769500f6f61ce2e13
sdaitzman <samuel@daitzman.com>
2016-06-07 12:23:10 -0400

    Revert "pre presentation mockup commit"
    This reverts commit 534a7761a4b1f94243403069ff7a6cef73ed7361.

--- www/js/libraries.js
+++ www/js/libraries.js
@@ -3,4 +3,3 @@
 //=include ../bower_components/lodash/dist/lodash.min.js
 //=include ../bower_components/transparency/dist/transparency.min.js
 //=include ../bower_components/select2/dist/js/select2.full.min.js
-//=include ../bower_components/three.js/build/three.min.js
